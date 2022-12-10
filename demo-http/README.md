23c1a2e39506679937693df2bdbdbc06743b3ff1
cfhui <jr2013hcf@163.com>
2022-12-10 18:20:47 +0800

    feat: 增加 http请求 工具类

---
+++ demo-http/README.md
@@ -0,0 +1,3 @@
+发送 http 请求相关
+1. apache http
+2. spring http (推荐)
---
+++ demo-http/src/main/java/com/cfhui/SpringBootDemoHttpApplication.java
@@ -0,0 +1,17 @@
+package com.cfhui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName ${NAME}
+ * @Description TODO
+ * @Author cfhui
+ * @Date ${DATE} ${TIME}
+ */
+@SpringBootApplication
+public class SpringBootDemoHttpApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoHttpApplication.class, args);
+    }
+}
