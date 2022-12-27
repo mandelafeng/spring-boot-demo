ff2f50b96b01eb5b5964b56de50c7644b2253f32
“huichunfeng” <huichunfeng@destech.cn>
2022-12-27 20:01:46 +0800

    feat: 增加业务响应码

---
+++ demo-restful/src/main/java/com/cfhui/constant/ResponseCodeConstant.java
@@ -0,0 +1,14 @@
+package com.cfhui.constant;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author cfhui
+ * @version V1
+ * @description TODO
+ * @date 2022/12/27 17:46
+ */
+@Slf4j
+public class ResponseCodeConstant {
+
+}
