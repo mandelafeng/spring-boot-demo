9ccb3d73ae3be83d4cc7c3ff7a01aceb4dc695aa
cfhui <jr2013hcf@163.com>
2023-03-12 14:29:31 +0800

    feat: 集成 sftp 2次提交

--- demo-sftp/src/main/java/com/cfhui/SpringBootDemoSftp.java
+++ demo-sftp/src/main/java/com/cfhui/SpringBootDemoSftp.java
@@ -1,8 +1,12 @@
 package com.cfhui;
 
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SpringBootDemoSftp {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(SpringBootDemoSftp.class, args);
     }
 }
