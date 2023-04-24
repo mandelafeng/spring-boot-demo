9aad0d662fc194f7dfd14a6f31710563147fcc66
cfhui <jr2013hcf@163.com>
2023-04-24 23:24:42 +0800

    feat: shell-判断当前系统是Ubuntu还是centos

--- bin/software_install.sh
+++ bin/software_install.sh
@@ -15,3 +15,26 @@ if git --version | grep 'git version [0-9]*\.[0-9]*\.[0-9]*'; then
     tar -zxvf git-2.22.0.tar.gz
 fi
 
+# 判断当前系统是Ubuntu还是centos
+if [ -f /etc/lsb-release ]; then
+    . /etc/lsb-release
+    OS=$DISTRIB_ID
+elif [ -f /etc/debian_version ]; then
+    OS=Debian
+elif [ -f /etc/redhat-release ]; then
+    OS="`cat /etc/redhat-release | sed s/.*release\ // | sed s/\ .*//`"
+else
+    OS="$(uname -s) $(uname -r)"
+fi
+case $OS in
+  "Ubuntu")
+    echo "This is Ubuntu"
+    ;;
+  "CentOS")
+    echo "This is CentOS"
+    ;;
+  *)
+    echo "This is neither Ubuntu nor CentOS"
+    ;;
+esac
+
