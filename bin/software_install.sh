e5b9098e7c50e6453f815074ea17c9bb614cb9de
cfhui <jr2013hcf@163.com>
2023-04-24 17:47:45 +0800

    feat: linux 检查git是否安装脚本

--- bin/software_install.sh
+++ bin/software_install.sh
@@ -1,3 +1,4 @@
+#! /bin/bash
 # 环境准备
 # 1. git
 # 2. jdk
@@ -7,3 +8,10 @@
 # 6. mysql
 
 # git
+if git --version | grep 'git version [0-9]*\.[0-9]*\.[0-9]*'; then
+  echo '已安装Git'
+  else
+    echo '需要安装git'
+    tar -zxvf git-2.22.0.tar.gz
+fi
+
