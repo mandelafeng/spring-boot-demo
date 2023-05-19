011c44ac9ca37c61c05fbd776b23f19607c5ee9c
cfhui <jr2013hcf@163.com>
2023-05-19 09:01:40 +0800

    test: 测试git

--- demo-java/src/test/java/com/cfhui/StreamAPITest.java
+++ demo-java/src/test/java/com/cfhui/StreamAPITest.java
@@ -123,6 +123,7 @@ public class StreamAPITest {
     public void test15() {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         Map<Boolean, List<Integer>> collect = list.stream().collect(Collectors.partitioningBy(i -> i % 2 == 0));
+
         System.out.println(collect);
     }
 
