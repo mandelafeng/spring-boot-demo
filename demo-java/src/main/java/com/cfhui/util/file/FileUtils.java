b5e58362c32273d75a4bf354a52d5c5a768176e8
cfhui <jr2013hcf@163.com>
2023-09-13 22:51:59 +0800

    fix: java-FileUtils 增加file multipartFile互转

--- demo-java/src/main/java/com/cfhui/util/file/FileUtils.java
+++ demo-java/src/main/java/com/cfhui/util/file/FileUtils.java
@@ -1,5 +1,10 @@
 package com.cfhui.util.file;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -81,7 +86,7 @@ public class FileUtils {
     /**
      * 文件读取
      *
-     * @param filePath 问价路径
+     * @param filePath 文件路径
      * @return 返回二进制
      * @author Lius
      * @date 2018/10/26 16:45
@@ -177,5 +182,56 @@ public class FileUtils {
             decompressor.end();
         }
         return bos.toByteArray();
+    }
+    public static File MultipartFileToFile(MultipartFile multipartFile) throws IOException {
+        File toFile = null;
+        if (!(multipartFile.getSize() <= 0)) {
+            InputStream ins = multipartFile.getInputStream();
+            toFile = new File(multipartFile.getOriginalFilename());
+            inputStreamToFile(ins, toFile);
+            ins.close();
+        }
+        return toFile;
+    }
+
+    private static void inputStreamToFile(InputStream ins, File file) {
+        try {
+            OutputStream os = Files.newOutputStream(file.toPath());
+            int bytesRead = 0;
+            byte[] buffer = new byte[8192];
+            while ((bytesRead = ins.read(buffer, 0, 8192)) != -1) {
+                os.write(buffer, 0, bytesRead);
+            }
+            os.close();
+            ins.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void deleteTempFile(File file){
+        if(file!=null){
+            File delFile = new File(file.toURI());
+            delFile.delete();
+        }
+    }
+    // 将File类转化为MultipartFile的方法
+    public static FileItem getMultipartFile(File file, String fieldName){
+        FileItemFactory factory = new DiskFileItemFactory(16, null);
+        FileItem item = factory.createItem(fieldName, "text/plain", true, file.getName());
+        int bytesRead = 0;
+        byte[] buffer = new byte[8192];
+        try {
+            FileInputStream fis = new FileInputStream(file);
+            OutputStream os = item.getOutputStream();
+            while ((bytesRead = fis.read(buffer, 0, 8192)) != -1) {
+                os.write(buffer, 0, bytesRead);
+            }
+            os.close();
+            fis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return item;
     }
 }
