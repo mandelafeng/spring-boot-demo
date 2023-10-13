83177ec999763fe3a40f7ac087cfe91c0c206e37
cfhui <jr2013hcf@163.com>
2023-10-13 14:58:03 +0800

    feat: 1.juc-CountDownLatch test

--- demo-java/src/main/java/com/cfhui/thread/CountDownlatch/CountDownLatchDemo.java
+++ demo-java/src/main/java/com/cfhui/thread/CountDownlatch/CountDownLatchDemo.java
@@ -9,10 +9,11 @@ import java.util.concurrent.CountDownLatch;
  * @version V1
  * @date 2023/10/12 下午 4:27
  */
-public class Demo {
+public class CountDownLatchDemo {
     public static void main(String[] args) {
         CountDownLatch countDownLatch = new CountDownLatch(1);
         new ThreadA(countDownLatch).start();
         new ThreadB(countDownLatch).start();
+        System.out.println("D");
     }
 }
