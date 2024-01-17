fd281d7e5d79cdfb9fc7fe4f701ef973321a0721
cfhui <jr2013hcf@163.com>
2024-01-17 17:20:53 +0800

    feat: 1.事务补充2

--- demo-orm-mybatis-plus/src/test/java/com/xkcoding/orm/mybatis/plus/service/TransactionPropagationTest.java
+++ demo-orm-mybatis-plus/src/test/java/com/xkcoding/orm/mybatis/plus/service/TransactionPropagationTest.java
@@ -4,9 +4,15 @@ import com.xkcoding.orm.mybatis.plus.SpringBootDemoOrmMybatisPlusApplicationTest
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionException;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.interceptor.TransactionAspectSupport;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.support.TransactionCallback;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
+import org.springframework.transaction.support.TransactionTemplate;
 
 /**
  * []
@@ -21,18 +27,53 @@ public class TransactionPropagationTest extends SpringBootDemoOrmMybatisPlusAppl
     @Autowired
     private TransactionPropagationServiceA service;
 
+    @Autowired
+    private PlatformTransactionManager platformTransactionManager;
+
+    @Autowired
+    private DataSourceTransactionManager transactionManager;
+
+    @Autowired
+    private TransactionTemplate transactionTemplate;
+
     @Test
     public void transaction() {
         service.main();
     }
 
-    @Autowired
-    private PlatformTransactionManager platformTransactionManager;
-
     @Test
     public void test1() {
         log.info("获取当前事务名称:" + TransactionSynchronizationManager.getCurrentTransactionName());
-        // 手动提交事务
+        // 手动提交事务？
         platformTransactionManager.commit(TransactionAspectSupport.currentTransactionStatus());
+
+        // -----------------------------------------------
+        DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+        def.setName("transA");
+        def.setPropagationBehavior(DefaultTransactionDefinition.PROPAGATION_REQUIRES_NEW);
+
+        TransactionStatus transaction = transactionManager.getTransaction(def);
+
+        try {
+            // 手动提交
+            transactionManager.commit(transaction);
+        } catch (TransactionException e) {
+            // 手动回滚
+            transactionManager.rollback(transaction);
+        }
+    }
+
+    @Test
+    public void test2() {
+        // 编程式事务
+      transactionTemplate.execute(new TransactionCallback<Object>() {
+          @Override
+          public Object doInTransaction(TransactionStatus transactionStatus) {
+              // 操作A
+              // 操作B
+              return null;
+          }
+      });
+
     }
 }
